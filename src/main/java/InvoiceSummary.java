943f133359a57a57f58e58d81bf92c7c7c11553a
vamsikrish-A <krishnavamsi165@gmail.com>
2021-10-06 23:37:57 +0530

    adding invoice summary

---
+++ src/main/java/InvoiceSummary.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class InvoiceSummary {
+    private final double numberOfRides;
+    private final double totalFare;
+    private final double averageFare;
+
+    public InvoiceSummary(int fare, double rides) {
+        this.numberOfRides = rides;
+        this.totalFare = fare;
+        this.averageFare = this.totalFare/ this.numberOfRides;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary summary = (InvoiceSummary) o;
+        return Double.compare(summary.numberOfRides, numberOfRides) == 0 && totalFare == summary.totalFare && Double.compare(summary.averageFare, averageFare) == 0;
+    }
+
+
+
+}
