a45006946f87270386adb71cb6b8ae8cba214986
vamsikrish-A <krishnavamsi165@gmail.com>
2021-10-06 20:50:09 +0530

    fare calculate by using TDD

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -1,3 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare,0.0);
+
+    }
 
 }
